8407e1bc30896a078c97ac89462a26ea30a24f22
Александр Рязанцев <amapkymap@gmail.com>
2022-06-09 00:35:06 +0300

    CsvFileSource commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,11 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class BonusServiceTest {
 
-    void shouldCalcForRegisteredAndOverLimit() {
+    @ParameterizedTest
+    @CsvFileSource(resources="/bonusService.csv")
+
+    public void shouldCalcExactBonus(long expected, long amount, boolean registered) {
+
         BonusService service = new BonusService();
 
-        long amount = 1000_60;
-        boolean registered = true;
+        long actual = service.calculate(amount, registered);
 
-          service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
     }
 }
